083c736404efabf0897e0a13a829495b880d6d5e
David Ek <david@ekgexpert.se>
2025-06-07 13:53:42 +0200

    Update supabaseClient.js steg 1

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -5,6 +5,16 @@ const supabase = createClient(
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
+async function getEventsFromSupabase() {
+  const { data, error } = await supabase
+    .from("events")
+    .select("eventid, eventraceid, eventdate");
+
+  if (error) throw error;
+  return data;
+}
+
 module.exports = {
   supabase,
+  getEventsFromSupabase,
 };
